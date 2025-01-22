860e19d2d0b0f147657d2d6330ce7fcc112a2807
nathanael <me@nattho.com>
2025-01-22 10:10:15 +0000

    Email headers changed

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,9 @@
+<tr>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            <img src="{{ url('img/email_icon.png') }}" class="logo" alt="Chirper Logo">
+            <br>
+            {{ config('app.name') }}
+        </a>
+    </td>
+</tr>
